a81bc7f1b3a62d8c99f7ce876a69fdcc353abd8b
NarenParra <naren21p@gmail.com>
2019-10-24 17:02:01 -0500

    moment cron para seguimiento

--- config/cron.js
+++ config/cron.js
@@ -1,6 +1,10 @@
+var moment = require('moment');
+moment.locale('es');
+var contador = 0;
 module.exports.cron = {
+
   myFirstJob: {
-    schedule: '7 * * * * *',
+    schedule: '* * * * * *',
     onTick: function () {
 
 
@@ -51,9 +55,52 @@ module.exports.cron = {
                       if (contratoetiqueta.descripcion == "En una fecha determinada posterior") {
                         console.log('entra fecha otra')
                         // va la logica de que hay que hacer con fecha posterior
-                        var periodo =contratoetiqueta
-                      }
-                      else if(contratoetiqueta.descripcion == "De forma periodica"){
+                        var unidad="";
+                        if (contratoetiqueta.unidadPeriodo == 'Días') {
+                          unidad= "days";
+                        } else if (contratoetiqueta.unidadPeriodo == 'Meses') { 
+                          unidad="month"
+                        } else if (contratoetiqueta.unidadPeriodo == 'Años') {
+                          unidad="year"
+                        }
+
+                  
+
+                        console.log("unidad")
+                        console.log(unidad)
+                        var inicio = moment(element.fechainicia).add(1,'days').format("YYYY-MM-DD");
+                        console.log("inicio")
+                        console.log(inicio)
+                        var inicioCantidad = moment(inicio).add(contratoetiqueta.cantidadPeriodo, unidad).format("YYYY-MM-DD");
+                        console.log("inicioCantidad")
+                        console.log(inicioCantidad)
+                        var now = moment().format("YYYY-MM-DD");
+                        var diferencia = moment(inicioCantidad).diff(inicio, 'days');
+                        console.log("diferencia")
+                        console.log(diferencia)
+
+                        console.log(contador++)
+
+                        if (diferencia >= 1 && diferencia <= 3) {
+                          console.log('debe pagar pronto')
+                          // cumo sacar los usuarios?
+
+                          // estructura Email
+                          // let mailOptions = {
+                          //   from: "vendedor@gmail.com",
+                          //   to: "comprador@gmail.com",
+                          //   subject: `Not a GDPR update ;)`,
+                          //   text: `Hi there, this email was automatically sent by us`
+                          // };
+                        } else if (moment(now).isSame(inicioCantidad)) {
+                          console.log('Dia del pago')
+                        } else if (moment(moment(now)).isAfter(inicioCantidad)) {
+                          console.log('Pago atrazado');
+
+                        }
+
+
+                      } else if (contratoetiqueta.descripcion == "De forma periodica") {
                         console.log('PERIODICA')
                       }
                     }
